49d0bfbdea0b08d8121f24a6616fc8ae2d631a0d
Denis Evdokimov <jabber89@mail.ru>
2016-05-10 00:52:01 +0300

    Main code refactoring.

--- api/controllers/NotificationController.js
+++ api/controllers/NotificationController.js
@@ -5,16 +5,21 @@
  * @help :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+const TEMPLATE_ERROR = 'Template not set error';
+
 module.exports = {
 	send : function(req, res) {
 		var template = req.param('template');
 		if (!template) {
 			return res.json({
-				error : 'Template not set'
+				error : TEMPLATE_ERROR
 			});
 		}		
-		Players.query('SELECT first_name, array_agg(vk_id) AS ids FROM players GROUP BY first_name', function(error, players) {					
-			return res.json(NotificationService.send(players.rows, template));					
-		});		
+		Players.query(
+			'SELECT first_name, array_agg(vk_id) AS ids FROM players GROUP BY first_name',			
+			function(error, players) {					
+				return res.json(NotificationService.send(players.rows, template));					
+			}
+		);		
 	}
 };
--- api/services/NotificationService.js
+++ api/services/NotificationService.js
@@ -25,94 +25,10 @@ var cookies = {
 	vkontakte: ''	
 }; 
 
-var sendAuthRequest = function(callback){	
-	request.post({
-		url: 'http://localhost:1338/sendNotification',		
-		json : true				       		
-	}, function(error, response, body) {							
-		try {													
-			if(error){
-				self.throwConnectionError();
-			}else if(response.statusCode >= 500){				
-				self.throwTooFreqError()										
-			}						
-			
-			var setCookie = setCookieParser.parse(response);
-			for(var i = 0; i < setCookie.length; i++){
-				if(setCookie[i].name == 'vkontakte'){
-					cookies.vkontakte = setCookie[i].value; 
-					break;
-				}
-			}								
-		}catch(e) {			
-			return callback(e, {});
-		}
-		return callback(null, {});
-	});
-};
-
-var sendRequest = function(players, template, callback){
-	var self = NotificationService;				
-	var text = template.replace('%name%', players.first_name);	
-	request.post({
-		url: 'http://localhost:1338/sendNotification',
-		headers: {
-			'Cookie': 'vkontakte=' + cookies.vkontakte 
-		},
-		formData : {
-			ids : players.ids,
-			text : text
-		},
-		json : true				       		
-	}, function(error, response, body) {				
-		var log = '';
-		var logStream = fs.createWriteStream('./logs/notification.log', {flags : 'a'});		
-		try {														
-			if(error){
-				self.throwConnectionError();
-			}else if(response.statusCode == 401){
-				self.throwConnectionError();
-			}else if(response.statusCode >= 500){
-				switch(body.code){
-					case 1:
-						self.throwTooFreqError();						
-					case 2:
-						self.throwServerError();
-					default:
-						self.throwServerError();
-				}						
-			}											
-			
-			var setCookie = setCookieParser.parse(response);
-			for(var i = 0; i < setCookie.length; i++){
-				if(setCookie[i].name == 'vkontakte'){
-					cookies.vkontakte = setCookie[i].value; 
-					break;
-				}
-			}
-			
-			log = 'Success: Send messages to users ' + players.first_name + ' Description: [' + body + '] \n';			
-		}catch(e) {
-			log = 'Error: Send messages to users ' + players.first_name + ' Description: ' + e.description + ' \n';								
-			return callback(e, {});
-		}finally {
-			logStream.write(log);
-			logStream.close();
-		}
-		return callback(null, {});
-	});
-};
-
-var sendTimeoutRequest = function(player, template, callback){
-	setTimeout(function(){	
-		console.log('Call timer');
-		sendRequest(player, template, callback);
-	}, 1000);
-};
-
 var PlayerIterator = function(playersData){
 	var self = this;
-	var limit = 100;				
+	const LIMIT = 100;
+	
 	this.i = 0;
 	this.j = 0;		
 	this.data = [];
@@ -123,11 +39,11 @@ var PlayerIterator = function(playersData){
 		if(players){
 			for(var i = 0; i <  players.length; i++){
 				var player = players[i];
-				var parts = player.ids.length / limit;
+				var parts = player.ids.length / LIMIT;
 				self.data[i] = [];
 				for(var j = 0; j < parts; j++){
 					self.data[i][j] = {};
-					var ids = player.ids.slice(j * limit, (j + 1) * limit);
+					var ids = player.ids.slice(j * LIMIT, (j + 1) * LIMIT);
 					self.data[i][j]['first_name'] = player.first_name;
 					self.data[i][j]['ids'] = ids;  
 				}			
@@ -141,9 +57,9 @@ var PlayerIterator = function(playersData){
 	
 	this.next = function(){
 		var i = this.i;
-		var j = this.j;		
-		j++;			
+		var j = this.j;	
 		
+		j++;					
 		try{
 			var player = this.data[i][j];				
 			if(!player){
@@ -167,6 +83,7 @@ var PlayerIterator = function(playersData){
 	this.get = function(){
 		var i = this.i;
 		var j = this.j;
+		
 		try{
 			var player = this.data[i][j];
 		}catch(e){
@@ -177,70 +94,160 @@ var PlayerIterator = function(playersData){
 	
 };
 
-var NotificationService = {
+var NotificationService = function(){
+	var self = this;
 	
-	generateServerError: function(){
+	this.generateServerError = function(){
 		return SERVER_ERROR; 
-	},
+	};
 	
-	generateTooFreqError: function(){
+	this.generateTooFreqError = function(){
 		return TOO_FREQ_ERROR;
-	},
+	};
 	
-	generateConnectionError: function(){
+	this.generateConnectionError = function(){
 		return CONNECTION_ERROR;
-	},
+	};
 	
-	throwConnectionError: function(){
+	this.throwConnectionError = function(){
 		throw SERVER_ERROR; 
-	},
+	};
 	
-	throwServerError: function(){
+	this.throwServerError = function(){
 		throw SERVER_ERROR; 
-	},
+	};
 	
-	throwTooFreqError: function(){
+	this.throwTooFreqError = function(){
 		throw TOO_FREQ_ERROR;
-	},
+	};
 	
-	throwConnectionError: function(){
+	this.throwConnectionError = function(){
 		throw CONNECTION_ERROR; 
-	},
+	};
+	
+	var sendAuthRequest = function(callback){	
+		request.post({
+			url: sails.config.services.vkontakte + 'sendNotification',		
+			json : true				       		
+		}, function(error, response, body) {							
+			try {													
+				if(error){
+					self.throwConnectionError();
+				}else if(response.statusCode >= 500){				
+					self.throwTooFreqError()										
+				}						
+				
+				var setCookie = setCookieParser.parse(response);
+				for(var i = 0; i < setCookie.length; i++){
+					if(setCookie[i].name == 'vkontakte'){
+						cookies.vkontakte = setCookie[i].value; 
+						break;
+					}
+				}								
+			}catch(e) {			
+				return callback(e, {});
+			}
+			return callback(null, {});
+		});
+	};
+	
+	var sendTimeoutRequest = function(player, template, callback){
+		setTimeout(function(){			
+			sendRequest(player, template, callback);
+		}, 1000);
+	};
+
+	var sendRequest = function(players, template, callback){
+		var self = NotificationService;				
+		var text = template.replace('%name%', players.first_name);
 		
-	init: function(){
-		var self = this;
-		console.log('Service init');
-		fs.exists(
-			sails.config.paths.tasks + '/notification.json',
-			function(isExist){
-				if(isExist){		
-					fs.readFile(
-						sails.config.paths.tasks + '/notification.json',
-						'utf8',
-						function(err, taskData){
-							if(!err){
-								taskData = JSON.parse(taskData);
-								var playerIterator = new PlayerIterator(taskData.iterator);
-								fs.unlink(sails.config.paths.tasks + '/notification.json');														
-								async.series([
-					              function(callback){
-					            	  sendAuthRequest(callback);            	  
-					              },
-					              function(callback){
-					            	  self.sendNotification(playerIterator, taskData.template, callback);            	  
-					              }
-					            ]);	
-							}
-						}
-					);
+		request.post({
+			url: sails.config.services.vkontakte + 'sendNotification',
+			headers: {
+				'Cookie': 'vkontakte=' + cookies.vkontakte 
+			},
+			formData : {
+				ids : players.ids,
+				text : text
+			},
+			json : true				       		
+		}, function(error, response, body) {				
+			var log = '';
+			var logStream = fs.createWriteStream(sails.config.paths.log, {flags : 'a'});		
+			try {														
+				if(error){
+					self.throwConnectionError();
+				}else if(response.statusCode == 401){
+					self.throwConnectionError();
+				}else if(response.statusCode >= 500){
+					switch(body.code){
+						case 1:
+							self.throwTooFreqError();						
+						case 2:
+							self.throwServerError();
+						default:
+							self.throwServerError();
+					}						
+				}											
+				
+				var setCookie = setCookieParser.parse(response);
+				for(var i = 0; i < setCookie.length; i++){
+					if(setCookie[i].name == 'vkontakte'){
+						cookies.vkontakte = setCookie[i].value; 
+						break;
+					}
+				}
+				
+				log = 'Success: Send messages to users ' + players.first_name + ' Description: [' + body + '] \n';			
+			}catch(e) {
+				log = 'Error: Send messages to users ' + players.first_name + ' Description: ' + e.description + ' \n';								
+				return callback(e, {});
+			}finally {
+				logStream.write(log);			
+			}
+			return callback(null, {});
+		});
+	};
+			
+	var restoreNotification = function(){
+		fs.readFile(
+			sails.config.paths.task,
+			'utf8',
+			function(err, taskData){
+				if(!err){
+					taskData = JSON.parse(taskData);
+					var playerIterator = new PlayerIterator(taskData.iterator);
+					fs.unlink(sails.config.paths.task);														
+					async.series([
+		              function(callback){
+		            	  sendAuthRequest(callback);            	  
+		              },
+		              function(callback){
+		            	  self.sendNotification(playerIterator, taskData.template, callback);            	  
+		              }
+		            ]);	
 				}
 			}
 		);
-	},			
+	};
 	
-	sendNotification: function(playerIterator, template, callback){
+	this.init = function(){
+		var self = this;
+		
+		fs.exists(
+			sails.config.paths.task,
+			function(isExist){
+				if(isExist){		
+					restoreNotification();
+				}
+			}
+		);
+	};			
+	
+	this.sendNotification = function(playerIterator, template, callback){
 		var self = this;
 		var player = null;
+		
 		async.whilst(function(){
 			if(!player){
 				player = playerIterator.get();
@@ -253,41 +260,39 @@ var NotificationService = {
 			return false;
 		},
 		function(callback){
-			console.log('Send request to player: ' + player.first_name);
 			sendTimeoutRequest(player, template, callback);
 		},
 		function(err, n){
 			if(err){
-				if(err.code == 1){
-					console.log('Repeat request');
+				if(err.code == 1){					
 					return self.sendNotification(playerIterator, template, callback);
-				}else if(err.code == 2){
-					console.log('Save state');
+				}else if(err.code == 2){					
 					return self.saveTask(playerIterator, template);
 				}
-			}
-			console.log('End');
+			}			
 		});
-	},
+	};
 	
-	saveTask: function(playerIterator, template){
+	this.saveTask = function(playerIterator, template){
 		var taskData = {
 			iterator: playerIterator,
 			template: template 
-		};		
-		taskData = JSON.stringify(taskData);		
-		var taskStream = fs.createWriteStream(sails.config.paths.tasks + '/notification.json', {flags : 'w'});
-		try {
-			taskStream.write(taskData);
-		}finally{
-			taskStream.close();
-		}				
-	},
+		};	
+		var taskStream = fs.createWriteStream(sails.config.paths.task, {flags : 'w'});
+		
+		taskData = JSON.stringify(taskData);						
+		taskStream.write(taskData);					
+	};
 	
-	send: function(players, template){
+	this.send = function(players, template){
 		var self = this;
 		var message = SERVICE_CALL_SUCCESS;
-		var playerIterator = new PlayerIterator({players: players});
+		var playerIterator = new PlayerIterator(
+			{
+				players: players
+			}
+		);
+		
 		try {				
 			async.series([
               function(callback){
@@ -303,8 +308,8 @@ var NotificationService = {
 		return {
 			message: message
 		};
-	}
+	};
 		
 };
 
-module.exports = NotificationService;
+module.exports = new NotificationService();
